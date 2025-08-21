208ae049fbc26c53c6bd619354ceb9624880a7bd
Muharrem Kaçkın <muharremkackin@strawhats.company>
2025-08-21 18:56:25 +0300

    unit equarable implemented

--- SH.Framework.Library.Cqrs/IRequest.cs
+++ SH.Framework.Library.Cqrs/IRequest.cs
@@ -4,8 +4,21 @@ public interface IRequest<out TResponse>;
 
 public interface IRequest : IRequest<Unit>;
 
-public struct Unit
+public struct Unit: IEquatable<Unit>
 {
+    public bool Equals(Unit other) => true;
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Unit other && Equals(other);
+    }
+
+    public override int GetHashCode() => 0;
+    
+    public static bool operator ==(Unit left, Unit right) => true;
+    public static bool operator !=(Unit left, Unit right) => false;
+    
+    public static Unit Value => default;
 }
 
 public delegate Task NotificationHandlerDelegate(CancellationToken cancellationToken = default);
